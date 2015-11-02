45021690680c4d793af512b6ffe44e15c51737e3
admin <bupt_z@xiaokunmacbook.local>
2015-11-02 17:51:59 +0800

    Initial Commit

---
+++ XMPPTemplet/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XMPPTemplet
+//
+//  Created by admin on 15/11/2.
+//  Copyright © 2015年 admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
